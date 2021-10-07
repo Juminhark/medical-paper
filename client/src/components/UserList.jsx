6afc960f054c018c361fcdc7dbec84ec344522cf
Ju minhark <wnalsgkr@gmail.com>
2021-10-07 10:19:07 +0900

    Update UserList.jsx

--- client/src/components/UserList.jsx
+++ client/src/components/UserList.jsx
@@ -3,8 +3,20 @@ import { Avatar, usChatChannel } from 'stream-chat-react';
 
 import { InviteIcon } from '../assets';
 
+const ListContainer = ({ children }) => {
+	return (
+		<div className="user-list__container">
+			<div className="user-list__header">
+				<p>User</p>
+				<p>Invite</p>
+			</div>
+			{children}
+		</div>
+	);
+};
+
 const UserList = () => {
-	return <div>UserList</div>;
+	return <ListContainer>UserList</ListContainer>;
 };
 
 export default UserList;
